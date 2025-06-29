0807c0c9cfb71d9c9626b97f70e6a5b927127216
Zahra Jane <144792966+janerysn@users.noreply.github.com>
2025-06-29 10:32:44 +0700

    Update theme.dart

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -4,17 +4,38 @@ class AppTheme {
   static final lightTheme = ThemeData(
     useMaterial3: true,
     colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.teal,
+      seedColor: const Color(0xFF262C4F),
       brightness: Brightness.light,
     ),
-    scaffoldBackgroundColor: Colors.white,
+    scaffoldBackgroundColor: const Color(0xFF2F3144),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFF2F3144),
+      foregroundColor: Colors.white,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color(0xFF262C4F),
+        foregroundColor: Colors.white,
+      ),
+    ),
   );
 
   static final darkTheme = ThemeData(
     useMaterial3: true,
     colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.teal,
+      seedColor: const Color(0xFF262C4F),
       brightness: Brightness.dark,
+    ),
+    scaffoldBackgroundColor: const Color(0xFF2F3144),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFF2F3144),
+      foregroundColor: Colors.white,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color(0xFF262C4F),
+        foregroundColor: Colors.white,
+      ),
     ),
   );
 }
